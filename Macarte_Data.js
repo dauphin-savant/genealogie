aec56afbc87480ce74fa83ea9e85e344e676638c
dauphin-savant <dauphin.savant@gmail.com>
2026-05-14 09:29:44 +0200

    Add files via upload

--- Macarte_Data.js
+++ Macarte_Data.js
@@ -2,6 +2,8 @@
 var mesAncetres = [
     {
         nom: "Jean Durand",
+		naissance: "1950",
+		villenaissance: "Nantes",
         date: "1820-1890",
         lieu: "Limoges",
         coords: [45.8336, 1.2611],
@@ -9,6 +11,8 @@ var mesAncetres = [
     },
     {
         nom: "Marie Morel",
+		naissance: "1960",
+		villenaissance: "Lyon",
         date: "1845-1910",
         lieu: "Lyon",
         coords: [45.7640, 4.8357],
@@ -16,6 +20,8 @@ var mesAncetres = [
     },
     {
         nom: "Pierre Lefebvre",
+		naissance: "1970",
+		villenaissance: "Cholet",
         date: "1790-1850",
         lieu: "Nantes",
         coords: [47.2184, -1.5536],
