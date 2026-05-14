0bc9ad3a8001767b6964800c0a51e7a2f1286b89
dauphin-savant <dauphin.savant@gmail.com>
2026-05-14 09:12:02 +0200

    Add files via upload

---
+++ Macarte_Data.js
@@ -0,0 +1,24 @@
+// On crée une variable globale accessible par l'autre script
+var mesAncetres = [
+    {
+        nom: "Jean Durand",
+        date: "1820-1890",
+        lieu: "Limoges",
+        coords: [45.8336, 1.2611],
+        details: "Maçon de la Creuse"
+    },
+    {
+        nom: "Marie Morel",
+        date: "1845-1910",
+        lieu: "Lyon",
+        coords: [45.7640, 4.8357],
+        details: "Couturière"
+    },
+    {
+        nom: "Pierre Lefebvre",
+        date: "1790-1850",
+        lieu: "Nantes",
+        coords: [47.2184, -1.5536],
+        details: "Marin-pêcheur"
+    }
+];
